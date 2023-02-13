d8fb56b2083f048ca4eae1b7536f4fcca2c2e232
Wenjie Zhang <19354817+myxxxsquared@users.noreply.github.com>
2023-02-13 08:33:02 +0000

    modify error

--- lua/myxxxsquared/plugins.lua
+++ lua/myxxxsquared/plugins.lua
@@ -29,6 +29,8 @@ local packer_spec = function(use)
     use "hrsh7th/nvim-cmp"
     use "hrsh7th/cmp-nvim-lsp"
     use "hrsh7th/cmp-path"
+    use "L3MON4D3/LuaSnip"
+    use "saadparwaiz1/cmp_luasnip"
     use "windwp/nvim-autopairs"
     use "numToStr/Comment.nvim"
     use "akinsho/bufferline.nvim"
